785a81c4d11b40c619371606df1a5ee4c04d83fc
Ryan Hirsch <ryan.hirsch@gmail.com>
2017-07-25 05:18:42 -0500

    add logging and flag for ignore weekend games

--- lambda/announce.js
+++ lambda/announce.js
@@ -2,12 +2,13 @@ const axios = require('axios');
 const oneDay = 86400000;
 
 function lambdaHandler(event, context, callback) {
-  return getGamesForDate(new Date(Date.now() + oneDay))
-    .then(games =>
-      Promise.all(
+  return getGamesForDate(new Date(Date.now() + oneDay), true)
+    .then(games => {
+      console.log('Games returned', games.length);
+      return Promise.all(
         games.map(sendMessage)
       )
-    )
+    })
     .then(() => {
       callback(null);
     })
@@ -20,6 +21,8 @@ function lambdaHandler(event, context, callback) {
 exports.handler = lambdaHandler;
 
 function sendMessage(game) {
+  const msg = `The ${game.home_team_name} are playing the ${game.away_team_name} @ ${game.home_time} tomorrow`;
+  console.log(msg);
   return axios.post(process.env.URL, {
     text: `The ${game.home_team_name} are playing the ${game.away_team_name} @ ${game.home_time} tomorrow`
   })
@@ -36,20 +39,25 @@ function padDate(val) {
   return val < 10 ? `0${val}` : `${val}`;
 }
 
-exports.getGamesForDate = function getGamesForDate(date) {
-  switch(date.getDay()) {
-    case 0:
-    case 6:
-      return Promise.resolve([]);
+function getGamesForDate(date, ignoreWeekendGames) {
+  if(ignoreWeekendGames) {
+    switch(date.getDay()) {
+      case 0:
+      case 6:
+        return Promise.resolve([]);
+    }
   }
   const url = `http://gd2.mlb.com/components/game/mlb/${getDateSegment(date)}/miniscoreboard.json`;
+  console.log('Getting games from', url);
   return axios.get(url)
     .then(({ data }) => {
       return data.data.games.game
-        .filter(g => g.location === 'St. Louis, MO')
-        .filter(g => {
-          const start = parseInt(g.home_time.split(':')[0], 10);
-          return !(start < 9 && start > 6);
-        })
+        .filter(g => g.location === 'St. Louis, MO');
     });
+}
+exports.getGamesForDate = getGamesForDate;
+
+function onlyDayGames(game) {
+  const start = parseInt(game.home_time.split(':')[0], 10);
+  return !(start < 9 && start > 6);
 }
